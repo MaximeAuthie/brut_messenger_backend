41b4ce6a1acfb1e52e415e12d29a30f4e525cecc
Maxime Authié <authie.maxime@orange.fr>
2023-06-02 11:14:24 +0200

    Ajout gestion des jwt

--- src/Controller/UserApiController.php
+++ src/Controller/UserApiController.php
@@ -10,34 +10,85 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use App\Entity\User;
-use App\Service\Utils;
+use App\Services\Utils;
+use App\Services\ApiAuthentification;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 //! Vérifier le projet SYmfony pour le use et la méthode à ajouter pour le hash du MDP
 
 class UserApiController extends AbstractController {
 
-    //! API pour renvoyer les données utilisateur (user profile) TRY CATCH????????
-    #[ROUTE('api/user/{id}', name:"app_api_user_id", methods: 'GET')]
-    public function getUserById(int $id, UserRepository $userRepository):Response {
-        //? On recherche l'utilisateur par son id dans la BDD
-        $user = $userRepository->find($id);
+    //! API pour renvoyer les données utilisateur (user profile)
+    #[ROUTE('api/user/id', name:"app_api_user_get", methods: 'GET')]
+    public function getUserById(Request $request, SerializerInterface $serializerInterface, UserRepository $userRepository, ApiAuthentification $apiAuthentification):Response {
+        try {
+            //? Récupérer le contenu de la requête en provenance du front
+            $json = $request->getContent();
 
-        //? On vérifie si $user est vide (s'il n'existe pas dans la BDD)
-        if (empty($user)) {
+            //? On vérifie que le json n'est pas vide
+            if (!$json) {
+                return $this->json(
+                    ['Error' => 'The json is empty or does not exist.'],
+                    400,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], 
+                    []
+                );
+            }
+
+            //? Serializer le json
+            $data = $serializerInterface->decode($json,'json');
+
+            //? Stocker les données du json dans des variables
+            $id = $data['id'];
+        
+            //? Récupérer les variables à passer en paramètre de la méthode verifyToken() de ApiAuthentification
+            $secretkey = $this->getParameter('token');
+            $token = $request->server->get('HTTP_AUTHORIZATION');
+            $token = str_replace('Bearer ', '', $token);
+
+            //? Appeller la méthode verifyToken() de ApiAuthentification
+            $checkToken = $apiAuthentification->verifyToken($token, $secretkey);
+            
+            //? Si la méthode verifyToken() retourne autre chose que true (une erreur)
+            if ($checkToken !== true) {
+                return $this->json(
+                    ['Error' => $checkToken],
+                    400, 
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], 
+                    []
+                );  
+            }
+
+            //? Rechercher l'utilisateur par son id dans la BDD
+            $user = $userRepository->find($id);
+
+            //? Vérifier si $user est vide (s'il n'existe pas dans la BDD)
+            if (empty($user)) {
+                return $this->json(
+                    ['Error' => 'This user does not exist'],
+                    206, 
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], 
+                    []
+                );
+            }
+
             return $this->json(
-                ['Erreur' => 'Cet utilisateur n\'existe pas dans la BDD'],
-                206, 
-                [], 
+                $user, 
+                200, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], //renvoie du json, uniquement depuis local host, et uniquelent sous forme de GET
+                ['groups' => 'user:getUserById']
+            );
+
+        } catch (\Exception $error) {
+
+            //? En cas d'erreur, on lève l'exception et on retourne le message d'erreur lié
+            return $this->json(
+                ['Error' => $error->getMessage()],
+                400, 
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], 
                 []
             );
         }
-
-        return $this->json(
-            $user, 
-            200, 
-            ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], //renvoie du json, uniquement depuis local host, et uniquelent sous forme de GET
-            ['groups' => 'user:getUserById']
-        );
     }
 
     //! API pour ajouter un utilisateur (inscription)
@@ -47,7 +98,7 @@ class UserApiController extends AbstractController {
             //? Récupérer le contenu de la requête en provenance du front
             $json = $request->getContent();
 
-            //? On vérifie que le body de la requête en provenance du front n'est pas vide
+            //? Vérifier que le body de la requête en provenance du front n'est pas vide
             if (!$json) {
                 return $this->json(
                     ['Erreur' => 'Le json est vide ou n\'esiste pas.'],
@@ -57,10 +108,10 @@ class UserApiController extends AbstractController {
                 );
             }
 
-            //? On serialize le json pour le transformer en taleau
+            //? Serializer le json pour le transformer en taleau
             $data = $serializerInterface->decode($json, 'json');
 
-            //? On vérifie si la date est valide
+            //? Vérifier si la date est valide
             if (!Utils::isValidDate($data['birthday'])) {
                 return $this->json(
                     ['Erreur' => 'L\a date '.$data['birthday'].' n\'est pas valide.'],
@@ -69,7 +120,7 @@ class UserApiController extends AbstractController {
                     [] );
             }
 
-            //? On vérifie si le format de l'adresse mail est valide
+            //? Vérifier si le format de l'adresse mail est valide
             if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
                 return $this->json(
                     ['Erreur' => 'L\'adresse mail '.$data['email'].' n\'est pas valide.'],
@@ -78,11 +129,11 @@ class UserApiController extends AbstractController {
                     [] );
             }
 
-            //? On vérifie si le user faisant l'objet de la demande existe déjà en BDD
+            //? Vérifier si le user faisant l'objet de la demande existe déjà en BDD
             $recup = $userRepository->findOneBy(['email'=>$data['email']]);
 
             if ($recup) {
-                //? On renvoie une erreur
+                //? Renvoyer une erreur
                 return $this->json(
                     ['Erreur' => 'L\'adresse mail '.$data['email'].' est déjà utilisée par un compte utilisateur.'],
                     206,
@@ -91,18 +142,18 @@ class UserApiController extends AbstractController {
                 );
             }
 
-            //? On nettoie les données envoyées par l'API
-            $firstName = Utils::cleanInput($data['firstName']);
-            $lastName = Utils::cleanInput($data['lastName']);
-            $birthday = Utils::cleanInput($data['birthday']);
-            $email = Utils::cleanInput($data['email']);
-            $password = Utils::cleanInput($data['password']);
-            $nickname = Utils::cleanInput($data['firstName']).' '.Utils::cleanInput($data['lastName']);
+            //? Nettoyer les données envoyées par l'API
+            $firstName  = Utils::cleanInput($data['firstName']);
+            $lastName   = Utils::cleanInput($data['lastName']);
+            $birthday   = Utils::cleanInput($data['birthday']);
+            $email      = Utils::cleanInput($data['email']);
+            $password   = Utils::cleanInput($data['password']);
+            $nickname   = Utils::cleanInput($data['firstName']).' '.Utils::cleanInput($data['lastName']);
 
-            //? On chiffre le password
+            //? Hasher le password
             $hashedPassword = password_hash($password, PASSWORD_DEFAULT); //! utiliseer hashPassword(objet user, variable MDP)
 
-            //? On instancie un objet User et on 'set' toutes ses propriétés
+            //? Instancier un objet User et on 'setter' toutes ses propriétés
             $user = new User;
             $user->setFirstNameUser($firstName);
             $user->setLastNameUser($lastName);
@@ -117,11 +168,11 @@ class UserApiController extends AbstractController {
             $user->setPublicKeyUser('1111111111');
             $user->setPrivateKeyUser('2222222222');
 
-            //? On persiste et flush les données de l'instance $user pour l'insérer en BDD
+            //? Persister et flush les données de l'instance $user pour l'insérer en BDD
             $entityManagerInterface->persist($user);
             $entityManagerInterface->flush();
 
-            //? On retourne un json pour avertir que l'enregistrement a réussit
+            //? Retourner un json pour avertir que l'enregistrement a réussit
             return $this->json(
                 ['erreur'=> 'Le compte '.$user->getEmail().' à bien été ajouté à la BDD.'],
                 200, 
@@ -131,7 +182,7 @@ class UserApiController extends AbstractController {
 
         } catch (\Exception $error) {
 
-            //? On retourne un json pour détailler l'erreur rencontrée
+            //? Retourner un json pour détailler l'erreur rencontrée
             return $this->json(
                 ['erreur'=> 'Etat du json : '.$error->getMessage()],
                 400, 
@@ -142,58 +193,83 @@ class UserApiController extends AbstractController {
     }
 
     //! API pour récupérer les données utilisateurs à la demande de connexion
-    #[ROUTE('api/user/logIn', name:"app_api_user_id", methods: 'GET')]
-    public function logInUser(UserRepository $userRepository, Request $request, SerializerInterface $serializerInterface ):Response {
-        //? On récupère le contenu de la requête en provenance du front
-        $json = $request->getContent();
+    #[ROUTE('api/user/logIn', name:"app_api_user_login", methods: 'GET')]
+    public function logInUser(ApiAuthentification $apiAuthentification, UserPasswordHasherInterface $userPasswordHasherInterface,  Request $request, SerializerInterface $serializerInterface, UserRepository $userRepository ):Response {
+        
+        try {
+            //? Récupérer le contenu de la requête en provenance du front
+            $json = $request->getContent();
 
-        //? On vérifie que le json n'est pas vide
-        if (!$json) {
+            //? On vérifie que le json n'est pas vide
+            if (!$json) {
+                return $this->json(
+                    ['Error' => 'Le json est vide ou n\'existe pas.'],
+                    400,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], 
+                    []
+                );
+            }
+
+            //? Serializer le json
+            $data = $serializerInterface->decode($json,'json');
+
+            //? Stocker les données du json dans des variables
+            $email      = $data['email'];
+            $password   = $data['password'];
+
+            //? Récupérer la clé de chiffrement
+            $secretkey = $this->getParameter('token');
+
+            //? Vérifier si les données du json ne sont pas vides
+            if (empty($email) OR empty($password)) {
+                return $this->json(
+                    ['Error' => 'One of the data is empty'],
+                    400,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], 
+                    []
+                );
+            }
+
+            //? Vérifier si le format de l'adresse mail est valide
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                return $this->json(
+                    ['Error' => 'Invalid e-mail address format'],
+                    400,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], 
+                    []
+                );
+            }
+            
+            //? Appeller la méthode d'authentification du service ApiAuthentification pour vérifier si on peut connecter l'utilisateur
+            if ($apiAuthentification->authentification($userPasswordHasherInterface ,$userRepository, $email, $password )) {
+
+                //? Si la méthode d'authentification retourne true, on génère un token avec la méthode genNewToken de ApiAuthentification
+                $token = $apiAuthentification->genNewToken($email, $secretkey, $userRepository);
+
+                return $this->json(
+                    $token, 
+                    200, 
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], //renvoie du json, uniquement depuis local host, et uniquelent sous forme de GET
+                    ['groups' => 'user:getUserById']
+                );
+            } else {
+                return $this->json(
+                    ['Error' => 'Wrong email or password'],
+                    401,
+                    ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], 
+                    []
+                );
+            }
+
+        } catch (\Exception $error){
+
+           //? En cas d'erreur, on lève l'exception et on retourne le message d'erreur lié
             return $this->json(
-                ['Erreur' => 'Le json est vide ou n\'esiste pas.'],
+                ['Error' =>$error->getMessage()],
                 400,
                 ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], 
                 []
             );
         }
-
-        //? On serialize le json
-        $data = $serializerInterface->decode($json,'json');
-
-        //? On recherche le user existe dans la BDD via son mail
-        $user = $userRepository->findOneBy(['email'=>$data['email']]);
-
-        //? Si l'utilisateur n'existe pas, on renvoie une erreur
-        if (!$user) {
-            return $this->json(
-                ['erreur'=> 'Adresse email ou mot de passe incorrect'],
-                400, 
-                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], //renvoie du json, uniquement depuis local host, et uniquelent sous forme de GET
-                []
-            );
-        }
-
-        //? Si l'utilisateur existe, on vérifie si sont mot de passe est correct
-        $inputPassword = $data['password'];
-
-        $databasePassword = $user->getPassword();
-
-        if (!password_verify($inputPassword, $databasePassword)) {  //! ERREUR ICI
-            return $this->json(
-                ['erreur'=> 'Adresse email ou mot de passe incorrect'],
-                400, 
-                ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], //renvoie du json, uniquement depuis local host, et uniquelent sous forme de GET
-                []
-            );
-        }
-
-        //? Si le mot de passe en correct, on retourne un json avec les données de l'utilisateur
-
-        return $this->json(
-            $user, 
-            200, 
-            ['Content-Type'=>'application/json','Access-Control-Allow-Origin' =>'localhost', 'Access-Control-Allow-Method' => 'GET'], //renvoie du json, uniquement depuis local host, et uniquelent sous forme de GET
-            ['groups' => 'user:getUserById']);
-
     }
 }
